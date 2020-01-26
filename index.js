a498f2037a2f92ef274fb5f3bb2930cf8df093c6
Edewor Onyedika <edwordkaso100@gmail.com>
2020-01-26 08:06:24 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res)=>{ 
+    res.render('index');
+});
+
+app.get('/about', (req, res)=>{
+    res.render('about');
+});
+
+app.listen(3000, ()=>{
+    console.log('Server listening on 3000');
+});
